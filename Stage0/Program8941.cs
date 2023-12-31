853f90efed4d43ef61579c98a365588abc56c02c
Khelkhely <rachel.shapiro.beitel@gmail.com>
2023-12-31 23:22:54 +0200

    function welcome8941

---
+++ Stage0/Program8941.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome8941()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
+
