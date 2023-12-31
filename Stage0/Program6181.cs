cc8c78952eec2b4d885afbf8a3592c7836822df7
tehiacohe <tehilacn0@gmail.com>
2023-12-31 22:45:56 +0200

    Initial Code +Welcome functions

---
+++ Stage0/Program6181.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome6181();
+            Welcome8941();
+
+        }
+        static partial void Welcome8941();
+        private static void Welcome6181()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + ", welcome to my first console application");
+        }
+    }
+}
