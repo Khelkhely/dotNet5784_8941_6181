c61208f440b752e164f3b3aa2ea93a85dc9df485
Khelkhely <rachel.shapiro.beitel@gmail.com>
2024-01-01 19:31:42 +0200

    up to ג3 tasks

---
+++ DalFacade/DO/Dependency.cs
@@ -0,0 +1,16 @@
+﻿namespace DO;
+/// <summary>
+/// 
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="DependentTask"></param>
+/// <param name="DependsOnTask"></param>
+public record Dependency
+(
+    int Id,
+    int DependentTask,
+    int DependsOnTask
+)
+{
+
+}
