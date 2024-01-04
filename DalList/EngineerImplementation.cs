dff8856ab698d497d57d497e304eccac32218fae
Khelkhely <rachel.shapiro.beitel@gmail.com>
2024-01-04 20:46:20 +0200

    CRUD functions for Engineer

--- DalList/EngineerImplementation.cs
+++ DalList/EngineerImplementation.cs
@@ -6,26 +6,40 @@ public class EngineerImplementation : IEngineer
 {
     public int Create(Engineer item)
     {
-        throw new NotImplementedException();
+        if (DataSource.Engineers.Exists(x => x.Id == item.Id))
+            throw new Exception($"Student with ID={item.Id} already exists");
+        DataSource.Engineers.Add(item);
+        return item.Id;
     }
 
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        if (DataSource.Engineers.Exists(x => x.Id == id))
+            DataSource.Engineers.RemoveAll(x => x.Id == id);
+        else
+            throw new Exception($"Engineer with ID={id} doesn't exist");
     }
 
     public Engineer? Read(int id)
     {
-        throw new NotImplementedException();
+        if (DataSource.Engineers.Exists(x => x.Id == id))
+            return DataSource.Engineers.Find(x => x.Id == id);
+        return null;
     }
 
     public List<Engineer> ReadAll()
     {
-        throw new NotImplementedException();
+        return new List<Engineer>(DataSource.Engineers);
     }
 
     public void Update(Engineer item)
     {
-        throw new NotImplementedException();
+        if (DataSource.Engineers.Exists(x => x.Id == item.Id))
+        {
+            DataSource.Engineers.RemoveAll(x => x.Id == item.Id);
+            DataSource.Engineers.Add(item);
+        }
+        else
+            throw new Exception($"Engineer with ID={item.Id} doesn't exist");
     }
 }
