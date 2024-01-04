f07f2bcf93ec129e81343829322b74904e178ea7
Khelkhely <rachel.shapiro.beitel@gmail.com>
2024-01-04 23:57:29 +0200

    CRUD functions for engineer

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -8,4 +8,14 @@ public static class Initialization
     private static IEngineer? s_dalEngineer; //stage 1
     private static IDependency? s_dalDependency; //stage 1
     private static readonly Random s_rand = new();
+
+    private static void createDpendency()
+    {
+        int range = s_dalTask!.ReadAll().Count;
+        int dependent = s_dalTask!.ReadAll()[s_rand.Next(range)].Id;
+        int dependsOn = s_dalTask!.ReadAll()[s_rand.Next(range)].Id;
+        
+        Dependency newDependency = new Dependency(0, dependent, dependsOn);
+
+    }
 }
