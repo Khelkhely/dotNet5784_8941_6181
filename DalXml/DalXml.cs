be6c25a5f99efa16076c372642845af6b241d4ef
tehiacohe <tehilacn0@gmail.com>
2024-01-21 22:48:12 +0200

    trying to fix xelement functions of dependency.

--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,18 +1,11 @@
 ﻿using DalApi;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace Dal;
 
-namespace Dal
+public sealed class DalXml : IDal
 {
-    public sealed class DalXml : IDal
-    {
-        public ITask Task =>  new TaskImplementation();
+    public ITask Task => new TaskImplementation();
 
-        public IDependency Dependency => new DependencyImplementation();
+    public IDependency Dependency => new DependencyImplementation();
 
-        public IEngineer Engineer => throw new NotImplementedException();
-    }
+    public IEngineer Engineer => new EngineerImplementation();
 }
