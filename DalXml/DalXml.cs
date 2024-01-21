60f056e90deb3db2a806fbc065b7d4467016b2c4
tehiacohe <tehilacn0@gmail.com>
2024-01-21 22:01:32 +0200

    start part 6

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,9 @@
+﻿using DalApi;
+namespace Dal;
+
+sealed public class DalXml
+{
+    public IEngineer Engineer => new EngineerImplementation();
+    public ITask Task => new TaskImlementation();
+    public IDependency Dependency => new DependencyImplementation();
+}
