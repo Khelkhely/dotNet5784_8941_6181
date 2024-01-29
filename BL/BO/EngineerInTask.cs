aa566168e20eed67ae3b2cc83979dd046b635407
Khelkhely <rachel.shapiro.beitel@gmail.com>
2024-01-29 16:10:26 +0200

    a

---
+++ BL/BO/EngineerInTask.cs
@@ -0,0 +1,17 @@
+﻿namespace BO;
+
+/// <summary>
+/// information about an engineer that is presented inside a task
+/// </summary>
+/// <param name="Id">Unique ID number</param>
+/// <param name="Name">Engineer's full name</param>
+public class EngineerInTask
+{
+    public int Id { get; init; }
+    public string? Name { get; set; }
+
+    public override string ToString()
+    {
+        return this.ToStringProperty();
+    }
+}
