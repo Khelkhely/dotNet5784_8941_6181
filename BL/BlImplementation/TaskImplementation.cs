a64f80d2b8f1eac4c2dfca8f35ec2588f48b49e5
Khelkhely <rachel.shapiro.beitel@gmail.com>
2024-01-30 23:52:54 +0200

    read task

--- BL/BlImplementation/TaskImplementation.cs
+++ BL/BlImplementation/TaskImplementation.cs
@@ -1,4 +1,5 @@
 ﻿using BlApi;
+using BO;
 
 namespace BlImplementation;
 
@@ -17,10 +18,23 @@ internal class TaskImplementation : ITask
 
     public BO.Task Read(int id)
     {
-        throw new NotImplementedException();
+        DO.Task d = _dal.Task.Read(id) ?? throw new BlDoesNotExistException($"Task with id: {id} doesn't exist");
+        BO.Task b = new BO.Task() { Id = id, 
+            Alias = d.Alias, 
+            Description = d.Description, 
+            CreatedAtDate = d.CreatedAtDate, 
+            ScheduledDate = d.ScheduledDate, 
+            StartDate = d.StartDate, 
+            DeadlineDate = d.DeadlineDate, 
+            CompleteDate = d.CompleteDate, 
+            Deliverables = d.Deliverables, 
+            Remarks = d.Remarks, Copmlexity = (BO.EngineerExperience)d.Complexity};
+        b.Status = CalculateStatus(b);
+        //TODO: Dependencies, Milestone, ForecastDate, RequiredEffortTime, Engineer
+        return b;
     }
 
-    public IEnumerable<BO.Task> ReadAll()
+    public IEnumerable<BO.Task> ReadAll(Func<BO.Task, bool>? filter = null)
     {
         throw new NotImplementedException();
     }
@@ -33,5 +47,18 @@ internal class TaskImplementation : ITask
     public void UpdateTaskDate(int id, DateTime date)
     {
         throw new NotImplementedException();
+    }
+
+    public Status CalculateStatus (BO.Task b)
+    {
+        if (b.ScheduledDate == null)
+            return Status.Unscheduled;
+        if (b.StartDate == null)
+            return Status.Scheduled;
+        if (b.CompleteDate == null)
+            return Status.OnTrack;
+        if (b.CompleteDate < DateTime.Today)
+            return Status.Done;
+        return b.Status = Status.InJeopardy;
     }
 }
