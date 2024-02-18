8dd01608dfd7ed4c24a9cffe27f7698057af769a
Khelkhely <rachel.shapiro.beitel@gmail.com>
2024-02-19 00:01:56 +0200

    up to part 7

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -19,9 +19,25 @@ namespace PL.Engineer
     /// </summary>
     public partial class EngineerListWindow : Window
     {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+
+
+        public IEnumerable<BO.Engineer> EngineerList
+        {
+            get { return (IEnumerable<BO.Engineer>)GetValue(EngineerListProperty); }
+            set { SetValue(EngineerListProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for EngineerList.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EngineerListProperty =
+            DependencyProperty.Register("EngineerList", typeof(IEnumerable<BO.Engineer>), typeof(EngineerListWindow), new PropertyMetadata(null));
+
+
         public EngineerListWindow()
         {
             InitializeComponent();
+            EngineerList = s_bl?.Engineer.ReadAll()!;
         }
     }
 }
---
+++ PL/Enums.cs
@@ -0,0 +1,13 @@
+﻿using BO;
+using System.Collections;
+
+namespace PL;
+
+public class EngineerExperienceLevel : IEnumerable<BO.EngineerExperience>
+{
+    static readonly IEnumerable<BO.EngineerExperience> s_enums =
+        (Enum.GetValues(typeof(BO.EngineerExperience)) as IEnumerable<BO.EngineerExperience>)!;
+
+    public IEnumerator<EngineerExperience> GetEnumerator() => s_enums.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => s_enums.GetEnumerator();
+}
